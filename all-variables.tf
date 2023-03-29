777fd15ab06be67a054a410359705e8849b00208
nir3shprabu <115976526+nir3shprabu@users.noreply.github.com>
2023-03-29 16:17:58 +0530

    ci: Remove all-variables.tf from gitignore

---
+++ all-variables.tf
@@ -0,0 +1,24 @@
+# gcp cloud infrastructure authentication details
+
+## Your project_id of your gcp account
+# Create a project in your gcp account and paste the id here
+project_id = "terraform-gcp-relayer"
+
+###################
+
+## Region 
+region = "us-central1"
+
+###################
+
+## Machine type of your gcp server
+machine_type = "e2-small"
+
+###################
+
+## Your SSH Details used to access the server
+# Fill in the path to the public key of the ssh key 
+public_keypath = "/home/nireshprabu/.ssh/id_rsa.pub"
+
+# Fill in the path to the private key of the ssh key 
+private_keypath = "/home/nireshprabu/.ssh/id_rsa"
